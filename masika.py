cc9e502420c08079b0cdfd84c15fe2baa0fd9407
jum648 <78852579+jum648@users.noreply.github.com>
2021-02-24 19:37:29 +0300

    Add files via upload

---
+++ masika.py
@@ -0,0 +1,28 @@
+print("this is python")
+a="10"
+b="20"
+print(a+b)
+print(a*3)
+a="jere"
+b="masika"
+c=a+b
+print(c)
+s="banana"
+print(s[1:4])
+print(s[1:5])
+print(s[:4])
+j=('mark ups')
+x=j.upper()
+print(x)
+a=("jerey BIG")
+x=a.lower()
+print(x)
+c=("marks omega")
+x=c.title()
+print(x)
+w='4'
+x=w.zfill(8)
+print(x)
+v=("lets start")
+x=v.index('lets')
+print(x)
